1810d713480eb52d31242ec40c6f36f0371b85c7
Ansat Euler <iffypixy@gmail.com>
2023-09-24 13:02:08 +0600

    feat: add tsconfig paths

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,6 +1,8 @@
 import {defineConfig} from "vite";
 import react from "@vitejs/plugin-react-swc";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
 });
+

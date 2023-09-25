2111181316f54381bb61453b894f57e27bfdfcbf
Ansat Euler <iffypixy@gmail.com>
2023-09-25 13:04:14 +0600

    feat: add font-family to tailwind config

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        body: "Inter, sans-serif",
+      },
+    },
   },
   plugins: [],
 };

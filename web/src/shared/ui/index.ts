df3eeaee129ce2e377df9451252b191512065ab6
Ansat Euler <iffypixy@gmail.com>
2023-09-29 00:43:05 +0600

    chore: add ui exports

---
+++ web/src/shared/ui/index.ts
@@ -0,0 +1,6 @@
+export {Icon} from "./icons";
+export {Text} from "./text";
+export {Button, UploadButton} from "./button";
+export {Container} from "./container";
+export {Skeleton} from "./skeleton";
+export {Logo} from "./logo";

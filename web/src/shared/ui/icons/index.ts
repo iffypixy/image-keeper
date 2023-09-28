df56ac273cc83fcbc0217acd12d49451308b6c0e
Ansat Euler <iffypixy@gmail.com>
2023-09-29 00:43:18 +0600

    feat: add icons

---
+++ web/src/shared/ui/icons/index.ts
@@ -0,0 +1,9 @@
+import Logo from "./logo.svg?react";
+import Upload from "./upload.svg?react";
+import Download from "./download.svg?react";
+import Edit from "./edit.svg?react";
+import Delete from "./delete.svg?react";
+import Cross from "./cross.svg?react";
+import Check from "./check.svg?react";
+
+export const Icon = {Logo, Upload, Download, Edit, Delete, Cross, Check};

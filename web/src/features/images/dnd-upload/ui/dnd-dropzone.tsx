9ab66ae9a3003226f67af579d367774fce504a38
Ansat Euler <iffypixy@gmail.com>
2023-09-30 01:55:13 +0600

    refactor: removed unnecessary modal

--- web/src/features/images/dnd-upload/ui/dnd-dropzone.tsx
+++ web/src/features/images/dnd-upload/ui/dnd-dropzone.tsx
@@ -1,14 +1,13 @@
 import React from "react";
 
-import {Modal} from "@shared/lib/modal";
 import {Nullable} from "@shared/lib/types";
 import {Text, Icon} from "@shared/ui";
 
-interface ImageUploaderModalProps {
+interface DNDDropzoneProps {
   onDrop: (event: DragEvent) => void;
 }
 
-export const DNDDropzone: React.FC<ImageUploaderModalProps> = (props) => {
+export const DNDDropzone: React.FC<DNDDropzoneProps> = (props) => {
   const [isDragging, setIsDragging] = React.useState(false);
 
   React.useEffect(() => {
@@ -49,15 +48,21 @@ export const DNDDropzone: React.FC<ImageUploaderModalProps> = (props) => {
     };
   }, []);
 
-  return (
-    <Modal open={isDragging}>
-      <div className="flex flex-col space-y-4 items-center text-center">
-        <Icon.Upload className="fill-[#A9E5BB] w-24 h-24" />
+  if (isDragging)
+    return (
+      <div className="fixed inset-0 z-50 flex items-center justify-center">
+        <div
+          role="presentation"
+          className="fixed inset-0 -z-10 bg-[rgba(255, 255, 255, 0.4)] backdrop-blur-lg"
+        />
 
-        <h2 className="text-3xl font-bold text-[#393E46]">Upload file</h2>
+        <div className="flex flex-col space-y-4 items-center text-center">
+          <Icon.Upload className="fill-[#A9E5BB] w-24 h-24" />
 
-        <Text color="secondary">Drag your file here to start uploading</Text>
+          <h2 className="text-3xl font-bold text-[#393E46]">Upload file</h2>
+
+          <Text color="secondary">Drag your file here to start uploading</Text>
+        </div>
       </div>
-    </Modal>
-  );
+    );
 };
